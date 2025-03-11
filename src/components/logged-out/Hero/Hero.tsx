cffe7c1a53eac502092bb288b3d167dc46582ea2
micoa <coma.comski@gmail.com>
2025-03-11 20:18:01 +0100

    css_bugfix

--- src/components/logged-out/Hero/Hero.tsx
+++ src/components/logged-out/Hero/Hero.tsx
@@ -11,44 +11,63 @@ const HeroSection = () => {
         <Container>
           <Row className="align-items-center hero-content">
             {/* Left Content */}
-            <Col
-              lg={6}
-              className="text-center text-lg-start text-white hero-first"
-            >
-              <h1 className="hero-title">
-                <span className="highlight">Klik</span>
-                <span className="highlight-2">Sigurnost</span>
-                <br /> za miran san roditelja i sigurnu budućnost dece.
-              </h1>
-              <p className="hero-subtitle">
-                Za roditelje na Balkanu koji žele da osiguraju bezbedno iskustvo
-                na internetu za svoju decu, KlikSigurnost nudi sveobuhvatnu
-                roditeljsku kontrolu i praćenje lokacije.
-              </p>
-              <Button variant="primary" className="hero-btn">
-                Besplatna registracija!
-              </Button>
-              {/* Available Platforms */}
-              <div className="platforms-area">
-                <div className="platforms mt-4">
-                  <span>Dostupno za sve uređaje:</span>
-                  <FaAndroid className="platform-icon" />
-                  <FaWindows className="platform-icon" />
-                  <FaApple className="platform-icon" />
+            <Col lg={6} md={12} className="text-center text-lg-start">
+              <div className="text-white hero-first">
+                <h1 className="hero-title">
+                  <div className="main-title">
+                    <span className="highlight">Klik</span>
+                    <span className="highlight-2">Sigurnost</span>
+                  </div>
+                  za miran san roditelja i sigurnu budućnost dece.
+                </h1>
+                <p className="hero-subtitle">
+                  Za roditelje na Balkanu koji žele da osiguraju bezbedno iskustvo
+                  na internetu za svoju decu, KlikSigurnost nudi sveobuhvatnu
+                  roditeljsku kontrolu i praćenje lokacije.
+                </p>
+                <Button variant="primary" className="hero-btn">
+                  Besplatna registracija!
+                </Button>
+                {/* Available Platforms */}
+                <div className="platforms-area">
+                  <div className="platforms mt-4">
+                    <span>Dostupno za sve uređaje:</span>
+                    <FaAndroid className="platform-icon" />
+                    <FaWindows className="platform-icon" />
+                    <FaApple className="platform-icon" />
+                  </div>
                 </div>
               </div>
             </Col>
 
-            {/* Right Image */}
-            <Col
-              lg={6}
-              className="text-center mx-auto text-center mt-4 mt-lg-0"
-            >
-              <img
-                src={heroImage}
-                alt="Hero Illustration"
-                className="hero-image img-fluid"
-              />
+            {/* Right Content */}
+            <Col lg={6} md={12} className="text-center text-lg-start">
+              <div className="text-white hero-first">
+                <h1 className="hero-title">
+                  <div className="main-title">
+                    <span className="highlight">Klik</span>
+                    <span className="highlight-2">Sigurnost</span>
+                  </div>
+                  za miran san roditelja i sigurnu budućnost dece.
+                </h1>
+                <p className="hero-subtitle">
+                  Za roditelje na Balkanu koji žele da osiguraju bezbedno iskustvo
+                  na internetu za svoju decu, KlikSigurnost nudi sveobuhvatnu
+                  roditeljsku kontrolu i praćenje lokacije.
+                </p>
+                <Button variant="primary" className="hero-btn">
+                  Besplatna registracija!
+                </Button>
+                {/* Available Platforms */}
+                <div className="platforms-area">
+                  <div className="platforms mt-4">
+                    <span>Dostupno za sve uređaje:</span>
+                    <FaAndroid className="platform-icon" />
+                    <FaWindows className="platform-icon" />
+                    <FaApple className="platform-icon" />
+                  </div>
+                </div>
+              </div>
             </Col>
           </Row>
         </Container>
