38a1f18674d998964542de7a11bf5ed3323bfbc7
micoa <coma.comski@gmail.com>
2025-03-31 15:52:57 +0200

    footer changes

--- src/components/logged-out/Footer/Footer.tsx
+++ src/components/logged-out/Footer/Footer.tsx
@@ -1,31 +1,39 @@
 import { Container, Row, Col } from "react-bootstrap";
 import {
   FaFacebookF,
-  FaLinkedinIn,
-  FaTwitter,
-  FaInstagram,
-  FaYoutube,
+  FaEnvelope,
+  FaFileContract,
 } from "react-icons/fa";
+import { Link } from "react-router-dom";
 import logo from "../../../logo_final2.png";
 
 const Footer = () => {
   return (
     <footer className="footer py-4 bg-light">
       <Container>
-        {/* Social Links on the Left, Logo in Center */}
         <Row className="align-items-center">
-          <Col
-            md={4}
-            sm={12}
-            className="text-md-start text-center social-links"
-          >
-            <FaFacebookF className="me-3" />
-            <FaLinkedinIn className="me-3" />
-            <FaTwitter className="me-3" />
-            <FaInstagram className="me-3" />
-            <FaYoutube />
+          {/* Social Links & Contact */}
+          <Col md={4} sm={12} className="text-md-start text-center">
+            <a href="https://www.facebook.com/profile.php?id=61573145025808" className="me-3 text-dark">
+              <FaFacebookF size={20} />
+            </a>
+            <span className="gap-3 me-3 text-dark">
+            <a href="mailto:podrska@kliksigurnost.com" className="text-dark">
+              <FaEnvelope size={20} />
+            </a>
+            </span>
+            <span className="">
+            <Link to="/privatnost" className="gap-3 me-3 text-dark">
+              <FaFileContract size={20} /> 
+            </Link>
+            </span>
+            <Link to="/uslovi" className="gap-3 me-3 text-dark">
+              <FaFileContract size={20} />
+            </Link>
+            <br/>
           </Col>
-
+          
+          {/* Logo */}
           <Col md={4} sm={12} className="text-center">
             <img
               src={logo}
@@ -33,10 +41,13 @@ const Footer = () => {
               width={80}
               className="mb-2"
             />
-          </Col>
 
+          </Col>
+          
+          {/* Legal Links */}
           <Col md={4} sm={12} className="text-md-end text-center">
             <p className="text-muted mb-0">KlikSigurnost © 2025</p>
+            
           </Col>
         </Row>
       </Container>
