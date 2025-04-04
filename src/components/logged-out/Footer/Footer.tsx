cd91ee1421537deee36598869ae272ab3ccfd1c9
micoa <coma.comski@gmail.com>
2025-04-04 15:15:49 +0200

    footer

--- src/components/logged-out/Footer/Footer.tsx
+++ src/components/logged-out/Footer/Footer.tsx
@@ -1,52 +1,45 @@
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  FaFacebookF,
-  FaEnvelope,
-  FaFileContract,
-} from "react-icons/fa";
+import { FaFacebookF, FaEnvelope, FaViber, FaWhatsapp } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import logo from "../../../logo_final2.png";
 
 const Footer = () => {
   return (
-    <footer className="footer py-4 bg-light">
+    <footer className="footer py-3 bg-light">
       <Container>
-        <Row className="align-items-center">
-          {/* Social Links & Contact */}
-          <Col md={4} sm={12} className="text-md-start text-center">
-            <a href="https://www.facebook.com/profile.php?id=61573145025808" className="me-3 text-dark">
+        <Row className="d-flex align-items-center justify-content-between flex-wrap">
+          {/* Social Links */}
+          <Col className="d-flex align-items-center gap-3">
+            <a href="https://www.facebook.com/profile.php?id=61573145025808" className="text-dark" target="_blank" rel="noopener noreferrer">
               <FaFacebookF size={20} />
             </a>
-            <span className="gap-3 me-3 text-dark">
             <a href="mailto:podrska@kliksigurnost.com" className="text-dark">
               <FaEnvelope size={20} />
             </a>
-            </span>
-            <span className="">
-            <Link to="/privatnost" className="gap-3 me-3 text-dark">
-              <FaFileContract size={20} /> 
-            </Link>
-            </span>
-            <Link to="/uslovi" className="gap-3 me-3 text-dark">
-              <FaFileContract size={20} />
-            </Link>
-            <br/>
-          </Col>
-          
-          {/* Logo */}
-          <Col md={4} sm={12} className="text-center">
-            <img
-              src={logo}
-              alt="KlikSigurnost Logo"
-              width={80}
-              className="mb-2"
-            />
+            <a href="viber://chat?number=%2B381601234567" className="text-dark" target="_blank" rel="noopener noreferrer">
+              <FaViber size={20} />
+            </a>
 
+            <a href="https://wa.me/381601234567" className="text-dark" target="_blank" rel="noopener noreferrer">
+              <FaWhatsapp size={20} />
+            </a>
           </Col>
-          
-          {/* Legal Links */}
-          <Col md={4} sm={12} className="text-md-end text-center">
+
+          {/* Logo */}
+          <Col className="d-flex flex-column justify-content-center align-items-center">
+            <img src={logo} alt="KlikSigurnost Logo" width={80} />
             <p className="text-muted mb-0">KlikSigurnost © 2025</p>
+          </Col>
+
+
+          {/* Copyright */}
+          <Col className="d-flex flex-column align-items-end">
+            <div className="mb-2">
+              <Link to="/uslovi" className="text-dark text-decoration-none me-2">Uslovi korištenja</Link>
+            </div>
+            <div className="mb-2">
+              <Link to="/privatnost" className="text-dark text-decoration-none">Politika privatnosti</Link>
+            </div>
             
           </Col>
         </Row>
