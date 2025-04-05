b5d2ef2d0e1f33147e4e8dade7856eee77ef9ff7
micoa <coma.comski@gmail.com>
2025-04-05 12:15:35 +0200

    build_error

--- src/components/logged-out/madeeasy/MadeEasy.tsx
+++ src/components/logged-out/madeeasy/MadeEasy.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { FaUserPlus, FaUsersCog, FaClock, FaShieldAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
